8a096991025e447442aa8bbced4e43c336bb787c
yecrystal <123497862+yecrystal@users.noreply.github.com>
2024-02-03 12:17:35 -0500

    Add files via upload

---
+++ popup/popup.js
@@ -0,0 +1,12 @@
+// BUTTON ELEMENTs
+
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+
+startButton.onclick = function() {
+    console.log("You clicked the start button!");
+};
+
+stopButton.onclick = function() {
+    console.log("You clicked the stop button!");
+};
